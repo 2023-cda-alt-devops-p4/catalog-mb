b9a664574c78eb4a305e31e1acceaad70467fb34
Beauvois Marine <marine.beauvois.pro@hotmail.com>
2023-09-20 16:28:50 +0200

    ajout home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@
 puts "Destroying diagram.."
 Diagram.destroy_all
 puts "Diagram destroyed"
+
+
+Comment.create!(content: "C'est génial, j'adore me déplacer en trotinette", user_id: first_user.id, post_id: post_1.id)
+
+Comment.create!(content: "J'ai pu économiser de l'argent depuis que j'utilise ce nouveau moyen de transport!,", user_id: first_user.id, post_id: post_2.id)
+
+Comment.create!(content: "C'est génial, j'adore me déplacer en gyropode", user_id: second_user.id, post_id: post_3.id)
+
+Comment.create!(content: "J'ai pu économiser de l'argent depuis que j'utilise ce nouveau moyen de transport!", user_id: second_user.id, post_id: post_4.id)
