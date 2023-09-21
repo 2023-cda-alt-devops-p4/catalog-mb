9e5611872dbfb71bacf9b29c5fb4b1c73ff854f6
mbeauvois <marine.beauvois.pro@hotmail.com>
2023-09-21 14:41:43 +0200

    update: modification seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-puts "Destroying diagram.."
-Diagram.destroy_all
-puts "Diagram destroyed"
+# Diagram.destroy_all
+# puts "Destroying diagram.."
 
-Diagram.create!(name: "Diagramme de séquence,", type: "UML," , description:"Il montre comment les objets interragissent les uns avec les autres")
-Diagram.create!(name: "Diagramme de classe,", type: "UML," , description:"Il représente les différentes classes dans un système et permet de comprendre la structure du système et les relation entre les parties et comprendre comment les faire avancer")
-Diagram.create!(name: "Diagramme de cas d'utilisation,", type: "UML," , description:"Il identifie les acteurs et les fonctionnalités d'un système")
-Diagram.create!(name: "Diagramme d'état,", type: "UML," , description:"Il représente les différents état d'un objet ou d'un coposant")
-Diagram.create!(name: "Diagramme de déploiement,", type: "UML," , description:"Il montre comment les composants logiciels sont déployés sur du matériel physique comme des serveurs ou des ordinateurs")
-Diagram.create!(name: "Diagramme de composant,", type: "UML," , description:"Il permet de représenter les composants logiciel et leurs dépendances")
-Diagram.create!(name: "Diagramme d'activité,", type: "UML," , description:"Il permet de connaître le comportement des composants")
-Diagram.create!(name: "Diagramme de communication,", type: "UML," , description:"Il permet de voir les déplacement des informations aux différents endroits")
-Diagram.create!(name: "Diagramme de présentation,", type: "UML," , description:"Il permet de faire fonctionner les composants par étape")
-Diagram.create!(name: "Diagramme de temps,", type: "UML," , description:"Il permet de connaître la durée de chaque étape")
+# Diagram.create!(name: "Diagramme de séquence,", type: "séquence,", description:"Il montre comment les objets interragissent les uns avec les autres", url:"....")
+# Diagram.create!(name: "Diagramme de classe,", type: "uml," , description:"Il représente les différentes classes dans un système et permet de comprendre la structure du système et les relation entre les parties et comprendre comment les faire avancer", url: "..")
+# Diagram.create!(name: "Diagramme de cas d'utilisation,", type: "uml," , description:"Il identifie les acteurs et les fonctionnalités d'un système", url:"...")
+# Diagram.create!(name: "Diagramme d'état,", type: "uml," , description:"Il représente les différents état d'un objet ou d'un coposant", url:"..")
+# Diagram.create!(name: "Diagramme de déploiement,", type: "uml," , description:"Il montre comment les composants logiciels sont déployés sur du matériel physique comme des serveurs ou des ordinateurs", url:"...")
+# Diagram.create!(name: "Diagramme de composant,", type: "uml," , description:"Il permet de représenter les composants logiciel et leurs dépendances", url:"..")
+# Diagram.create!(name: "Diagramme d'activité,", type: "uml," , description:"Il permet de connaître le comportement des composants", url:"..")
+# Diagram.create!(name: "Diagramme de communication,", type: "uml," , description:"Il permet de voir les déplacement des informations aux différents endroits", url:"..")
+# Diagram.create!(name: "Diagramme de présentation,", type: "uml," , description:"Il permet de faire fonctionner les composants par étape", url:"..")
+# Diagram.create!(name: "Diagramme de temps", type: "uml" , description:"Il permet de connaître la durée de chaque étape", url:"..")
