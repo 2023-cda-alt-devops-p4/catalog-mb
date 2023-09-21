33b084284eca6ef3b1b26058c9f100f5ac79e7cc
mbeauvois <marine.beauvois.pro@hotmail.com>
2023-09-21 16:00:20 +0200

    update :seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# Diagram.destroy_all
-# puts "Destroying diagram.."
+Diagram.destroy_all
+puts "Destroying diagram.."
 
-# Diagram.create!(name: "Diagramme de séquence,", type: "séquence,", description:"Il montre comment les objets interragissent les uns avec les autres", url:"....")
-# Diagram.create!(name: "Diagramme de classe,", type: "uml," , description:"Il représente les différentes classes dans un système et permet de comprendre la structure du système et les relation entre les parties et comprendre comment les faire avancer", url: "..")
+# -----------------------------------Création des diagrammes---------------------------------------------------------
+puts "Creating diagram.."
+
+one_diagram = Diagram.create!(name: "Diagramme de séquence,", type: "uml,", description:"Il montre comment les objets interragissent les uns avec les autres", url:"rfjkjgjdg")
+# diagram_2 = Diagram.create!(name: "Diagramme de classe,", type: "uml," , description:"Il représente les différentes classes dans un système et permet de comprendre la structure du système et les relation entre les parties et comprendre comment les faire avancer", url: "sdhbhvhsdjk")
 # Diagram.create!(name: "Diagramme de cas d'utilisation,", type: "uml," , description:"Il identifie les acteurs et les fonctionnalités d'un système", url:"...")
 # Diagram.create!(name: "Diagramme d'état,", type: "uml," , description:"Il représente les différents état d'un objet ou d'un coposant", url:"..")
 # Diagram.create!(name: "Diagramme de déploiement,", type: "uml," , description:"Il montre comment les composants logiciels sont déployés sur du matériel physique comme des serveurs ou des ordinateurs", url:"...")
@@ -18,3 +21,4 @@
 # Diagram.create!(name: "Diagramme de communication,", type: "uml," , description:"Il permet de voir les déplacement des informations aux différents endroits", url:"..")
 # Diagram.create!(name: "Diagramme de présentation,", type: "uml," , description:"Il permet de faire fonctionner les composants par étape", url:"..")
 # Diagram.create!(name: "Diagramme de temps", type: "uml" , description:"Il permet de connaître la durée de chaque étape", url:"..")
+puts "diagram created"
