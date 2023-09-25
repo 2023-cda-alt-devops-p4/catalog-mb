cb1eff2cc0b333fb3ce15ff7b450fd909e7c0c67
mbeauvois <marine.beauvois.pro@hotmail.com>
2023-09-25 10:19:38 +0200

    update: pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+  skip_before_action only: [ :home ]
   def home
+    @diagram = Diagram.all
   end
 end
+# -------affichage des diagrammes dans la home page-------
