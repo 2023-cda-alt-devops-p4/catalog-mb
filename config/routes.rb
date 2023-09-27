a8393890688ef77286b74a347808e7813b48c100
mbeauvois <marine.beauvois.pro@hotmail.com>
2023-09-27 10:20:30 +0200

    ci: add link to index and show merise and diagram

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
+
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :diagram, only: ['index', 'show']
-
-end
-
-# get 'pages/home'
+    resources :merises, only: [:index, :show]
+    resources :diagrams, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
