3f7901bdcdad139615c1aca50b33d99a41143bcc
mbeauvois <marine.beauvois.pro@hotmail.com>
2023-09-28 19:07:38 +0200

    feat: add info json

--- app/controllers/diagrams_controller.rb
+++ app/controllers/diagrams_controller.rb
@@ -1,11 +1,19 @@
 class DiagramsController < ApplicationController
 
+
   def index
-    @diagram = Rails.root.join('db/diagrams.json').read
+    @diagram = JSON.parse Rails.root.join('db/diagrams.json').read
   end
 
   def show
-    @diagram = Rails.root.join('db/diagrams.json').read
+    @diagram = JSON.parse Rails.root.join('db/diagrams.json').read
   end
+
+
+
+  # def diagram_params
+  #   params.require(:diagram).permit(:id)
+  # end
+
 
 end
